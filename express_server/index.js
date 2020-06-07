ecaf90e298e01fd3a00d92c0ada2d71cc14e563e
Enrique Lozano <elozano@law.harvard.edu>
2020-06-07 17:47:15 -0400

    Add Middlware

--- express_server/index.js
+++ express_server/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const createdMiddleware = (req, res, next) => {
+  console.log('Middleware has been run!');
+  next();
+};
+
+const requestedTimeMiddleware = (req, res, next) => {
+  req.requestedTime = Date.now();
+  next();
+};
+
+app.use(createdMiddleware);
+app.use(requestedTimeMiddleware);
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send(`Hello world!<br /> Some other ${req.requestedTime}`);
 });
 
 app.listen(port, () => {
